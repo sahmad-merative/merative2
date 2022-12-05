f1469467b717b2f3a18e49e523732bab2e7de980
Kunwar <saluja@adobe.com>
2022-12-05 14:59:31 -0500

    autoblock tags

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -104,6 +104,40 @@ export function getMetadata(name) {
   return $meta && $meta.content;
 }
 
+function buildTags(main) {
+  function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+  }
+
+  const tagsElement = document.createElement('div');
+  tagsElement.classList.add('tags');
+
+  const category = getMetadata('category');
+  let audience = getMetadata('audience');
+  let topic = getMetadata('topic');
+  let tags = [];
+  if (category) {
+    tags.push(category);
+    if (audience && topic) {
+      audience = audience.split(',').map((element) => element.trim());
+      topic = topic.split(',').map((element) => element.trim());
+      tags = tags.concat(audience, topic);
+    }
+  }
+  const uniqueTags = tags.filter(onlyUnique);
+  uniqueTags.forEach((item) => {
+    const tag = createTag('span', { class: 'tag' }); // can be changed to buttons to be clickable
+    tag.innerHTML = item;
+    tagsElement.append(tag);
+  });
+  const firstH2 = main.querySelector('h2:first-of-type');
+  const p = main.querySelector('p:first-of-type');
+  // eslint-disable-next-line no-bitwise
+  if (firstH2 && p && (firstH2.compareDocumentPosition(p) & Node.DOCUMENT_POSITION_FOLLOWING)) {
+    firstH2.append(tagsElement);
+  }
+}
+
 /**
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
@@ -113,6 +147,7 @@ function buildAutoBlocks(main) {
     buildHeroBlock(main);
     buildCtaBlock(main);
     buildBackToTopBlock(main);
+    buildTags(main);
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
