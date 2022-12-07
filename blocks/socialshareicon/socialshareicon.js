c5b8e7ead79cb81e23dd1778f7f6688d60a76b90
RoronoaSrinu <92785510+RoronoaSrinu@users.noreply.github.com>
2022-12-07 19:12:24 +0530

    concatenation update

--- blocks/socialshareicon/socialshareicon.js
+++ blocks/socialshareicon/socialshareicon.js
@@ -1,15 +1,16 @@
 function openLink(e) {
   const idName = e.target.getAttribute('id');
   let url = null;
+  let currentUrl = window.location.href;
   if (idName !== null) {
     if (idName.includes('linkedIn')) {
-      url = "https://www.linkedin.com/sharing/share-offsite/?url=" + window.location.href;
+      url = `https://www.linkedin.com/sharing/share-offsite/?url= ${currentUrl}`;
       window.open(url, '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=300,width=800,height=500');
     } else if (idName.includes('twitter')) {
-      url = "https://twitter.com/share?url=" + window.location.href;
+      url = `https://twitter.com/share?url= ${currentUrl}`;
       window.open(url, '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=300,width=800,height=500');
     } else if (idName.includes('facebook')) {
-      url = "https://www.facebook.com/sharer/sharer.php?u=" + window.location.href;
+      url = `https://www.facebook.com/sharer/sharer.php?u= ${currentUrl}`;
       window.open(url, '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=300,width=800,height=500');
     } else if (idName.includes('shareLink')) {
       navigator.clipboard.writeText(window.location.href);
@@ -20,7 +21,7 @@ function openLink(e) {
 function setFocus(e) {
   let idName = e.target.getAttribute('id');
   const link = e.target.parentNode.querySelectorAll('a');
-  link.forEach(a => {
+  link.forEach((a) => {
     const linkId = a.getAttribute('id');
     if (linkId == idName) {
       e.target.classList.add('active');
@@ -81,7 +82,7 @@ export default function decorate(block) {
   const childrenDiv = document.getElementById('blog-right-nav').querySelectorAll('div');
   const childrenH2 = document.getElementById('blog-right-nav').querySelectorAll('h2');
   const childrenH3 = document.getElementById('blog-right-nav').querySelectorAll('h3');
-  childrenDiv.forEach(div => {
+  childrenDiv.forEach((div) => {
     blogContent.append(div);
     /*if (w <= 768) {
     block.parentNode.setAttribute('id', 'wrapper');
@@ -96,12 +97,12 @@ export default function decorate(block) {
     blogContent.append(div);
     }*/
   });
-  childrenH2.forEach(h2 => {
+  childrenH2.forEach((h2) => {
     content_link_id = h2.getAttribute('id');
     content_link = h2.textContent;
     AnchorTagLinkCreation(content_link_id, content_link, block);
   });
-  childrenH3.forEach(h3 => {
+  childrenH3.forEach((h3) => {
     content_link_id = h3.getAttribute('id');
     content_link = h3.textContent;
     AnchorTagLinkCreation(content_link_id, content_link, block);
