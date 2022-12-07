087d018d7128cf7a315212e8cd856d1f0d6c1b91
RoronoaSrinu <92785510+RoronoaSrinu@users.noreply.github.com>
2022-12-07 18:59:38 +0530

    updating indent

--- blocks/socialshareicon/socialshareicon.js
+++ blocks/socialshareicon/socialshareicon.js
@@ -2,18 +2,18 @@ function openLink(e) {
   const idName = e.target.getAttribute('id');
   let url = null;
   if (idName !== null) {
-      if (idName.includes('linkedIn')) {
-          url = "https://www.linkedin.com/sharing/share-offsite/?url=" + window.location.href;
-          window.open(url, '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=300,width=800,height=500');
-      } else if (idName.includes('twitter')) {
-          url = "https://twitter.com/share?url=" + window.location.href;
-          window.open(url, '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=300,width=800,height=500');
-      } else if (idName.includes('facebook')) {
-          url = "https://www.facebook.com/sharer/sharer.php?u=" + window.location.href;
-          window.open(url, '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=300,width=800,height=500');
-      } else if (idName.includes('shareLink')) {
-          navigator.clipboard.writeText(window.location.href);
-      }
+    if (idName.includes('linkedIn')) {
+      url = "https://www.linkedin.com/sharing/share-offsite/?url=" + window.location.href;
+      window.open(url, '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=300,width=800,height=500');
+    } else if (idName.includes('twitter')) {
+      url = "https://twitter.com/share?url=" + window.location.href;
+      window.open(url, '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=300,width=800,height=500');
+    } else if (idName.includes('facebook')) {
+      url = "https://www.facebook.com/sharer/sharer.php?u=" + window.location.href;
+      window.open(url, '_blank', 'toolbar=yes,scrollbars=yes,resizable=yes,top=100,left=300,width=800,height=500');
+    } else if (idName.includes('shareLink')) {
+      navigator.clipboard.writeText(window.location.href);
+    }
   }
 }
 
@@ -21,16 +21,19 @@ function setFocus(e) {
   let idName = e.target.getAttribute('id');
   const link = e.target.parentNode.querySelectorAll('a');
   link.forEach(a => {
-      const linkId = a.getAttribute('id');
-      if (linkId == idName){
-          e.target.classList.add('active');
-      }
-      else{
-          document.getElementById(linkId).classList.remove('active');
-      }
+    const linkId = a.getAttribute('id');
+    if (linkId == idName) {
+      e.target.classList.add('active');
+    } else {
+      document.getElementById(linkId).classList.remove('active');
+    }
   });
   idName = idName.replace('-leftnav', '');
-  document.getElementById(idName).scrollIntoView({ behavior: "smooth", block: "start", inline: "end" });
+  document.getElementById(idName).scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "end"
+  });
 }
 
 function AnchorTagLinkCreation(content_link_id, content_link, block) {
@@ -79,29 +82,29 @@ export default function decorate(block) {
   const childrenH2 = document.getElementById('blog-right-nav').querySelectorAll('h2');
   const childrenH3 = document.getElementById('blog-right-nav').querySelectorAll('h3');
   childrenDiv.forEach(div => {
-      blogContent.append(div);
-      /*if (w <= 768) {
-      block.parentNode.setAttribute('id', 'wrapper');
-      blogContent.append(div);
-      if (div.getAttribute('data-block-name') == 'author-bio') {
-      const socialSharenDiv = document.getElementById('wrapper').querySelectorAll('div');
-      socialSharenDiv.forEach(div => {
-      blogContent.append(div);
-      });
-      }
-      } else {
-      blogContent.append(div);
-      }*/
+    blogContent.append(div);
+    /*if (w <= 768) {
+    block.parentNode.setAttribute('id', 'wrapper');
+    blogContent.append(div);
+    if (div.getAttribute('data-block-name') == 'author-bio') {
+    const socialSharenDiv = document.getElementById('wrapper').querySelectorAll('div');
+    socialSharenDiv.forEach(div => {
+    blogContent.append(div);
+    });
+    }
+    } else {
+    blogContent.append(div);
+    }*/
   });
   childrenH2.forEach(h2 => {
-      content_link_id = h2.getAttribute('id');
-      content_link = h2.textContent;
-      AnchorTagLinkCreation(content_link_id, content_link, block);
+    content_link_id = h2.getAttribute('id');
+    content_link = h2.textContent;
+    AnchorTagLinkCreation(content_link_id, content_link, block);
   });
   childrenH3.forEach(h3 => {
-      content_link_id = h3.getAttribute('id');
-      content_link = h3.textContent;
-      AnchorTagLinkCreation(content_link_id, content_link, block);
+    content_link_id = h3.getAttribute('id');
+    content_link = h3.textContent;
+    AnchorTagLinkCreation(content_link_id, content_link, block);
   });
   //  document.getElementById('wrapper').remove();
   document.getElementById('blog-right-nav').remove();
