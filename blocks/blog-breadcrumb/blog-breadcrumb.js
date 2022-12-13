8bbb334785506920d65bb591d84e9a0b48b18808
Srinivasula Reddy S <sirap@adobe.com>
2022-12-13 23:33:51 +0530

    updating changes

--- blocks/blog-breadcrumb/blog-breadcrumb.js
+++ blocks/blog-breadcrumb/blog-breadcrumb.js
@@ -1,365 +1,63 @@
-import {
-  sampleRUM,
-  buildBlock,
-  loadHeader,
-  loadFooter,
-  decorateButtons,
-  decorateIcons,
-  decorateSections,
-  decorateBlocks,
-  decorateTemplateAndTheme,
-  waitForLCP,
-  loadBlocks,
-  loadCSS,
-  createOptimizedPicture,
-} from './lib-franklin.js';
+import { getMetadata } from '../../scripts/scripts.js';
+let currentPathName = window.location.pathname;
+currentPathName = currentPathName.replace('/blog/', '');
 
-const LCP_BLOCKS = ['hero']; // add your LCP blocks to the list
-window.hlx.RUM_GENERATION = 'project-1'; // add your RUM generation information here
-
-function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', {
-      elems: [picture, h1]
-    }));
-    main.prepend(section);
-  }
+function openLink(e) {
+    let idName = e.target.getAttribute('id');
+    let url = null;
+    idName = idName.replace('-breadcrumb', '');
+    if (idName === 'Merative Blog') { // blog page
+        window.open('https://www.merative.com/blog', '_self');
+    } else if (idName === document.title) { // current page
+        // do nothing
+    } else { // category page
+        let pathName = idName.replace(/ /g, '-');
+        pathName = pathName.toLowerCase();
+        url = `https://www.merative.com/blog/${pathName}`;
+        window.open(url, '_self');
+    }
+    e.preventDefault();
 }
 
-function buildCtaBlock(main) {
-  main.querySelectorAll(':scope > div').forEach((div) => {
-    const h2 = div.querySelector('div > h2');
-    const p = div.querySelector('div > p');
-    const pa = div.querySelector('p > a');
-    const numChildren = div.children.length;
-
-    // simple CTA - no inner text and H2 positioned before a link
-    if (h2 && p && pa && (h2.compareDocumentPosition(pa) === 4)
-       && (numChildren === 2)) {
-      div.classList.add('cta');
-    }
-
-    // CTA with inner text -  H2 then text then a link
-    if (h2 && p && pa && (h2.compareDocumentPosition(p) === 4)
-       && (p.compareDocumentPosition(pa) === 4)
-       && (h2.compareDocumentPosition(pa) === 4)
-       && (numChildren === 3)) {
-      div.classList.add('cta');
-    }
-  });
-}
-
-function buildBackToTopBlock(main) {
-  const element = document.createElement('div');
-  element.classList.add('back-to-top');
-  main.append(element);
-
-  const backToTop = main.querySelector(':scope > div.back-to-top');
-  // add scroll listener
-  window.addEventListener('scroll', () => {
-    const scrollAmount = window.scrollY;
-    if (scrollAmount > 100) {
-      backToTop.classList.add('active');
+function breadCrumbCreation(id, name, ulFlag) {
+    let ul = null;
+    let linkText = null;
+    if (ulFlag) {
+        ul = document.createElement('ul');
+        ul.classList.add('breadcrumb-navigation');
+        ul.setAttribute('id', 'crumbs-list-id');
     } else {
-      backToTop.classList.remove('active');
+        ul = document.getElementById('crumbs-list-id');
     }
-  });
-
-  // add click listener
-  backToTop.addEventListener('click', () => {
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: 'smooth',
-    });
-  });
-}
-
-/**
- * Helper function to create DOM elements
- * @param {string} tag DOM element to be created
- * @param {array} attributes attributes to be added
- */
-
-export function createTag(tag, attributes, html) {
-  const el = document.createElement(tag);
-  if (html) {
-    if (html instanceof HTMLElement || html instanceof SVGElement) {
-      el.append(html);
+    var li = document.createElement('li');
+    const aLink = document.createElement('a');
+    if (mobileCheck() && ulFlag) {
+        linkText = document.createTextNode('...');
     } else {
-      el.insertAdjacentHTML('beforeend', html);
+        linkText = document.createTextNode(name);
     }
-  }
-  if (attributes) {
-    Object.entries(attributes).forEach(([key, val]) => {
-      el.setAttribute(key, val);
-    });
-  }
-  return el;
+    const sid = `${name}-breadcrumb`;
+    aLink.href = '#';
+    aLink.append(linkText);
+    aLink.setAttribute('id', sid);
+    aLink.href = '#';
+    li.appendChild(aLink);
+    ul.appendChild(li);
+    document.getElementById(id).append(ul);
+    document.getElementById(sid).addEventListener('click', openLink);
 }
 
-/**
- * Retrieves the content of a metadata tag.
- * @param {string} name The metadata name (or property)
- * @returns {string} The metadata value
- */
-export function getMetadata(name) {
-  const attr = name && name.includes(':') ? 'property' : 'name';
-  const $meta = document.head.querySelector(`meta[${attr}="${name}"]`);
-  return $meta && $meta.content;
+window.mobileCheck = function() {
+    let check = false;
+    (function(a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
+};
+
+
+export default function decorate(block) {
+    block.textContent = '';
+    block.setAttribute('id', 'breadCrumb-id');
+    breadCrumbCreation(block.getAttribute('id'), 'Merative Blog', true); // pass true to create first time ul element 
+    breadCrumbCreation(block.getAttribute('id'), getMetadata('category'), false); // pass false to use created ul element from previous function call.
+    breadCrumbCreation(block.getAttribute('id'), document.title, false); // pass false to use created ul element from previous function call.
 }
-
-function buildTags(main) {
-  const tagsElement = document.createElement('div');
-  const category = getMetadata('category');
-  tagsElement.classList.add('tags');
-  if (category) {
-    tagsElement.append(buildBlock('tags', {
-      elems: []
-    }));
-    const firstH2 = main.querySelector('h2:first-of-type');
-    const p = main.querySelector('p:first-of-type');
-    // eslint-disable-next-line no-bitwise
-    if (firstH2 && p && (firstH2.compareDocumentPosition(p) & Node.DOCUMENT_POSITION_FOLLOWING)) {
-      firstH2.after(tagsElement);
-    }
-  }
-}
-
-function buildPageDivider(main) {
-  const allPageDivider = main.querySelectorAll('code');
-
-  allPageDivider.forEach((el) => {
-    const alt = el.innerText.trim();
-    const lower = alt.toLowerCase();
-    if (lower === 'divider') {
-      el.innerText = '';
-      el.classList.add('divider');
-    }
-  });
-}
-// auto block build for social media share as left nav
-function buildSocialIconBlock(main) {
-  const isBlogPage = /(\/blog\/.*)/.test(window.location.pathname);
-  if (isBlogPage) {
-    const blogLeftNav = document.createElement('div');
-    blogLeftNav.classList.add('blog-left-nav');
-    blogLeftNav.append(buildBlock('blog-left-nav', {
-      elems: [],
-    }));
-    main.children[0].setAttribute('id', 'blog-right-nav');
-    main.prepend(blogLeftNav);
-  }
-}
-
-
-// auto block to create breadcrumb for blog articles
-function buildBlogBreadCrumbBlock(main) {
-  const isBlogPage = /(\/blog\/.*)/.test(window.location.pathname);
-  if (isBlogPage) {
-    const blogBreadCrumb = document.createElement('div');
-    blogBreadCrumb.classList.add('blog-breadcrumb');
-    blogBreadCrumb.append(buildBlock('blog-breadcrumb', {
-      elems: [],
-    }));
-    main.prepend(blogBreadCrumb);
-  }
-}
-
-/**
- * Builds all synthetic blocks in a container element.
- * @param {Element} main The container element
- */
-function buildAutoBlocks(main) {
-  try {
-    buildHeroBlock(main);
-    buildCtaBlock(main);
-    buildBackToTopBlock(main);
-    buildSocialIconBlock(main);
-    buildBlogBreadCrumbBlock(main);
-    buildTags(main);
-    buildPageDivider(main);
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error('Auto Blocking failed', error);
-  }
-}
-
-/**
- * Gets details about pages that are indexed
- * @param {Array} pathnames list of pathnames
- */
-
-export async function lookupPages(pathnames) {
-  if (!window.pageIndex) {
-    const resp = await fetch(`${window.hlx.codeBasePath}/query-index.json`);
-    const json = await resp.json();
-    const lookup = {};
-    json.data.forEach((row) => {
-      lookup[row.path] = row;
-      if (row.image || row.image.startsWith('/default-meta-image.png')) row.image = `/${window.hlx.codeBasePath}${row.image}`;
-    });
-    window.pageIndex = {
-      data: json.data,
-      lookup
-    };
-  }
-  const result = pathnames.map((path) => window.pageIndex.lookup[path]).filter((e) => e);
-  return (result);
-}
-
-/**
- * Gets details about blogs that are indexed
- * @param {Array} pathnames list of pathnames
- */
-
-export async function lookupBlogs(pathnames) {
-  if (!window.blogIndex) {
-    const resp = await fetch(`${window.hlx.codeBasePath}/blog-index.json`);
-    const json = await resp.json();
-    const lookup = {};
-    json.data.forEach((row) => {
-      lookup[row.path] = row;
-      if (row.image || row.image.startsWith('/default-meta-image.png')) row.image = `/${window.hlx.codeBasePath}${row.image}`;
-    });
-    window.blogIndex = {
-      data: json.data,
-      lookup
-    };
-  }
-  const result = pathnames.map((path) => window.blogIndex.lookup[path]).filter((e) => e);
-  return (result);
-}
-
-/**
- * Creates a Card using a JSON object and style associated with the card
- * @param {Object} row JSON Object typically coming from an index array item
- * @param {String} style Class name that needs to be added to the card root div
- */
-
-export async function createCard(row, style) {
-  // Create card div
-  const card = document.createElement('div');
-  if (style) card.classList.add(style);
-
-  // Add the image to the card first
-  if (row.image !== '0' && row.title !== '0') {
-    const cardImage = document.createElement('div');
-    cardImage.classList.add('card-image');
-    cardImage.append(createOptimizedPicture(row.image, row.title));
-    card.prepend(cardImage);
-  }
-
-  // Create a separate child div for the card content
-  const cardContent = document.createElement('div');
-  cardContent.classList.add('card-content');
-
-  // Create and add the link, title, author, readtime and category to card content and card
-  const link = document.createElement('a');
-  link.classList.add('blog-link');
-  link.href = row.path;
-  if (row.title) link.innerHTML += `${row.title}`;
-  cardContent.append(link);
-  if (row.description && row.description !== '0') cardContent.innerHTML += `<p>${row.description}</p>`;
-  const author = document.createElement('div');
-  author.classList.add('blog-author');
-  if (row.author && row.author !== '0') author.innerHTML += `By ${row.author}`;
-  if (row.readtime && row.readtime !== '0') author.innerHTML += ` | ${row.readtime}`;
-  cardContent.append(author);
-  const category = document.createElement('div');
-  category.classList.add('blog-category');
-  if (row.category && row.category !== '0') category.innerHTML += row.category;
-  cardContent.append(category);
-
-  card.append(cardContent);
-  return (card);
-}
-
-/**
- * Decorates the main element.
- * @param {Element} main The main element
- */
-// eslint-disable-next-line import/prefer-default-export
-export function decorateMain(main) {
-  // hopefully forward compatible button decoration
-  decorateButtons(main);
-  decorateIcons(main);
-  buildAutoBlocks(main);
-  decorateSections(main);
-  decorateBlocks(main);
-}
-
-/**
- * loads everything needed to get to LCP.
- */
-async function loadEager(doc) {
-  document.documentElement.lang = 'en';
-  decorateTemplateAndTheme();
-  const main = doc.querySelector('main');
-  if (main) {
-    decorateMain(main);
-    await waitForLCP(LCP_BLOCKS);
-  }
-}
-
-/**
- * Adds the favicon.
- * @param {string} href The favicon URL
- */
-export function addFavIcon(href) {
-  const link = document.createElement('link');
-  link.rel = 'icon';
-  link.type = 'image/svg+xml';
-  link.href = href;
-  const existingLink = document.querySelector('head link[rel="icon"]');
-  if (existingLink) {
-    existingLink.parentElement.replaceChild(link, existingLink);
-  } else {
-    document.getElementsByTagName('head')[0].appendChild(link);
-  }
-}
-
-/**
- * loads everything that doesn't need to be delayed.
- */
-async function loadLazy(doc) {
-  const main = doc.querySelector('main');
-  await loadBlocks(main);
-
-  const {
-    hash
-  } = window.location;
-  const element = hash ? main.querySelector(hash) : false;
-  if (hash && element) element.scrollIntoView();
-
-  loadHeader(doc.querySelector('header'));
-  loadFooter(doc.querySelector('footer'));
-
-  loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
-  addFavIcon(`${window.hlx.codeBasePath}/styles/favicon.svg`);
-  sampleRUM('lazy');
-  sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
-  sampleRUM.observe(main.querySelectorAll('picture > img'));
-}
-
-/**
- * loads everything that happens a lot later, without impacting
- * the user experience.
- */
-function loadDelayed() {
-  // eslint-disable-next-line import/no-cycle
-  window.setTimeout(() => import('./delayed.js'), 3000);
-  // load anything that can be postponed to the latest here
-}
-
-async function loadPage() {
-  await loadEager(document);
-  await loadLazy(document);
-  loadDelayed();
-}
-
-loadPage();
