d1fb8dcdbacf0e7312b64d92cf1b62198af8a72a
Anthony Rumsey <arumsey@adobe.com>
2023-02-06 16:47:08 -0500

    Leadspace (Hero) #98
    - add common button styling to styles.css
    - add video variant to leadspace
    - add play video button to leadspace

--- blocks/leadspace/leadspace.js
+++ blocks/leadspace/leadspace.js
@@ -4,44 +4,91 @@
  */
 
 export default function decorate(block) {
-  const firstCol = block.firstElementChild?.children.item(0);
+  const col1 = block.firstElementChild?.children.item(0);
+  const col2 = block.firstElementChild?.children.item(1);
+  const hasVideo = block.classList.contains('video');
 
-  // group buttons
-  const buttonGroup = document.createElement('div');
-  buttonGroup.classList.add('button-group');
+  // decorate video
+  if (hasVideo) {
+    // video lead space blocks are a special case that do not follow normal processing rules
+    if (col1) {
+      // watch video button
+      const videoLink = col1.querySelector('p > strong > a');
+      const videoDuration = col1.querySelector('p:last-of-type');
 
-  const buttons = [...block.querySelectorAll('.button-container')] || [];
+      const watchBtn = document.createElement('button');
+      watchBtn.setAttribute('type', 'button');
+      watchBtn.setAttribute('aria-label', 'Play video');
+      watchBtn.innerHTML = `<span><span>${videoLink.textContent}</span><span>${videoDuration.textContent}</span></span><span></span>`;
+      const btnContent = watchBtn.querySelector('span:nth-child(1)');
+      btnContent.classList.add('video-button-content');
+      btnContent.querySelector('span:nth-child(1)').classList.add('video-button-title');
+      btnContent.querySelector('span:nth-child(2)').classList.add('video-button-duration');
+      watchBtn.querySelector(':scope > span:nth-child(2)').classList.add('video-button-icon');
 
-  buttons.forEach((button) => {
-    const isPrimary = button.querySelector('strong');
-    const isSecondary = button.querySelector('em');
-    // position button
-    if (isPrimary) {
-      button.style.order = '1';
-    } else if (isSecondary) {
-      button.style.order = '2';
-    } else {
-      button.style.order = '3';
+      watchBtn.addEventListener('click', () => {
+        alert('I was clicked!');
+      })
+
+      col1.removeChild(videoLink.parentElement.parentElement);
+      col1.removeChild(videoDuration);
+
+      col1.appendChild(watchBtn);
     }
-    buttonGroup.append(button);
-  });
 
-  if (firstCol && buttonGroup.children.length) {
-    firstCol.append(buttonGroup);
+    if (col2) {
+      // preview link
+      const previewLink = col2.querySelector('a');
+      const video = document.createElement('div');
+      video.classList.add('video-wrapper');
+      video.innerHTML = `<video autoplay controls muted playsinline loop preload="auto">
+        <source src="${previewLink.href}" type="video/mp4">
+        </video>`;
+      col2.replaceChild(video, previewLink);
+    }
+    return;
   }
 
-  // group events
-  const eventItems = [...block.querySelectorAll('.leadspace.event p:not(.button-container) > em')] || [];
+  // Default (aka non-video) processing
 
-  const eventGroup = document.createElement('div');
-  eventGroup.classList.add('event-group');
+  if (col1) {
+    // group buttons
+    const buttonGroup = document.createElement('div');
+    buttonGroup.classList.add('button-group');
 
-  eventItems.forEach((item, index) => {
-    item.parentElement?.classList.add('event-item', index === 0 ? 'location' : 'date');
-    eventGroup.appendChild(item.parentElement);
-  });
+    const buttons = [...col1.querySelectorAll('.button-container')] || [];
 
-  if (firstCol && eventGroup.children.length) {
-    firstCol.append(eventGroup);
+    buttons.forEach((button) => {
+      const isPrimary = button.querySelector('strong');
+      const isSecondary = button.querySelector('em');
+      // position button
+      if (isPrimary) {
+        button.style.order = '1';
+      } else if (isSecondary) {
+        button.style.order = '2';
+      } else {
+        button.style.order = '3';
+      }
+      buttonGroup.append(button);
+    });
+
+    if (buttonGroup.children.length) {
+      col1.append(buttonGroup);
+    }
+
+    // group events
+    const eventItems = [...col1.querySelectorAll('.leadspace.event p:not(.button-container) > em')] || [];
+
+    const eventGroup = document.createElement('div');
+    eventGroup.classList.add('event-group');
+
+    eventItems.forEach((item, index) => {
+      item.parentElement?.classList.add('event-item', index === 0 ? 'location' : 'date');
+      eventGroup.appendChild(item.parentElement);
+    });
+    if (eventGroup.children.length) {
+      col1.append(eventGroup);
+    }
   }
+
 }
