4ed2ce4f34499516c5d40193e2ad022506701f9a
Anthony Rumsey <arumsey@adobe.com>
2023-05-10 14:13:20 -0400

    Columns block throws errors when adding alt text #253 (#132)
    * Columns block throws errors when adding alt text #253
    
    - add optional chaining to avoid runtime errors
    - use `forEach` instead of `map` with `querySelectorAll`
    
    * Columns block throws errors when adding alt text #253
    
    - add optional chaining to avoid runtime errors
    - use `forEach` instead of `map` with `querySelectorAll`

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,17 +1,20 @@
 export default function decorate(block) {
-  const isContentBand = block.classList.contains('content-band');
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
+  // Add alt text to images based on column type
+  const isContentBand = block.classList.contains('content-band');
   if (isContentBand) {
     block.querySelectorAll('.only-picture').forEach((el) => {
-      const content = el.parentElement.querySelector('h3, h2, h1').textContent || 'Content Band preview image';
+      const content = el.parentElement.querySelector('h3, h2, h1')?.textContent || 'Content Band preview image';
       el.querySelector('img').alt = content;
     });
   } else {
     cols.forEach((col) => {
-      const content = col.querySelector('h1, h2, h3, h4, h5').textContent || 'Column preview image';
-      col.querySelectorAll('img').map((img) => { img.alt = content; return img; });
+      const content = col.querySelector('h1, h2, h3, h4, h5')?.textContent || 'Column preview image';
+      col.querySelectorAll('img').forEach((img) => {
+        img.alt = content;
+      });
     });
   }
 }
