a99ff21cf0a97ef3050df84373516923668e4f83
Saad Ahmad <sahmed3@merative.com>
2023-06-22 16:42:06 +0530

    fixed lint issue

--- blocks/linkable-cards/linkable-cards.js
+++ blocks/linkable-cards/linkable-cards.js
@@ -2,17 +2,17 @@ export default async function decorate(block) {
   let html = '';
   if ([...block.children].length > 0) {
     [...block.children].forEach((element) => {
-        const content = element.children[0].innerHTML;
-        const lastColumn = element.children[element.children.length - 1];
-        const link = lastColumn.querySelectorAll('a')[0].getAttribute('href');
-        html += `<div><a href="${link}">${content}</a></div>`;
-        block.innerHTML = html;
+      const content = element.children[0].innerHTML;
+      const lastColumn = element.children[element.children.length - 1];
+      const link = lastColumn.querySelectorAll('a')[0].getAttribute('href');
+      html += `<div><a href="${link}">${content}</a></div>`;
+      block.innerHTML = html;
     });
   } else { // fetchign documents and populating top 3 cards.
     const resp = await fetch(`${window.hlx.codeBasePath}/documents/query-index.json`);
     const json = await resp.json();
     [...json.data].forEach((element, index) => {
-      if(index < 3) {
+      if (index < 3) {
         html += `<div><a href="e${element.path}"><p>${element.parent}</p>
         <h3 id="for-contract-research-organizations">${element.title}</h3>
         <p>${element.description}</p></a></div>`;
