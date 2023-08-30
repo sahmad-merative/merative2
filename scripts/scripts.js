5c29399836336a9d0909505b1ddcac0950a5d221
Putra Bonaccorsi <putra.roeung@gmail.com>
2023-08-30 11:10:51 -0400

    feat(quick-links): Add POC LinkTree/Bitly template for showing quick links (#318)
    * feat(quick-links): add quick links buttons block
    
    * feat(quick-links): add blocks and styles
    
    * feat(quick-links): add more style fixes and cleanup
    
    * feat(quick-links): clean up js block exports
    
    * feat(quick-links): add changes based on design review
    
    * feat(quick-links): adjust header logo mobile height
    
    * feat(quick-links): clean up code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -19,11 +19,12 @@ const LCP_BLOCKS = ['leadspace', 'blog-home']; // add your LCP blocks to the lis
 window.hlx.RUM_GENERATION = 'merative'; // add your RUM generation information here
 
 /**
- * Determine if we are serving content for the block-library, if so don't load the header or footer
- * @returns {boolean} True if we are loading block library content
+ * Determine if we are serving content for a specific keyword
+ * @param {string} keyword - The keyword to check in the URL path
+ * @returns {boolean} True if we are loading content for the specified keyword
  */
-export function isBlockLibrary() {
-  return window.location.pathname.includes('block-library');
+export function locationCheck(keyword) {
+  return window.location.pathname.includes(keyword);
 }
 
 /**
@@ -884,7 +885,7 @@ async function loadLazy(doc) {
   const element = hash ? main.querySelector(hash) : false;
   if (hash && element) element.scrollIntoView();
 
-  if (!isBlockLibrary()) {
+  if (!locationCheck('block-library') && !locationCheck('quick-links')) {
     loadHeader(doc.querySelector('header'));
     loadFooter(doc.querySelector('footer'));
   }
