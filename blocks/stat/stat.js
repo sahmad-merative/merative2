8f386711f3d29a731ef4397e2c0b7b9e29be6ee7
Saad Ahmad <sahmed3@merative.com>
2023-08-31 15:28:07 +0530

    fix(stats-bar): review comments fixes

--- blocks/stat/stat.js
+++ blocks/stat/stat.js
@@ -1,19 +1,29 @@
 /**
+ * set max-width to element
+ * @param {Element[]} elements Child nodes in which class of respective max-width add
+ * @param {string} className The class name for respective max-width
+ */
+function setMaxWidth(elements, className) {
+  [...elements].forEach((ele) => {
+    ele.classList.add(className);
+  });
+}
+
+/**
  * decorates the stat
  * @param {Element} block The stat block element
  */
 export default async function decorate(block) {
-  if (window.screen.width >= 768 && window.screen.width < 1200) {
+  const { width } = window.screen;
+
+  if (width >= 768 && width < 1200) {
     const statParent = block.firstElementChild;
     const childLenth = statParent.children.length;
+
     if (childLenth === 3) {
-      [...statParent.children].forEach((ele) => {
-        ele.style.maxWidth = 'calc((100%/3) - 16px)';
-      });
+      setMaxWidth(statParent.children, 'max-width-third');
     } else if (childLenth === 2) {
-      [...statParent.children].forEach((ele) => {
-        ele.style.maxWidth = 'calc(50% - 12px)';
-      });
+      setMaxWidth(statParent.children, 'max-width-half');
     } else {
       statParent.classList.add('no-effect');
     }
