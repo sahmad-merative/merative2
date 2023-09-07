7ecc266f1298cd491e7052d9658a1c52a3d800b9
Putra Bonaccorsi <putra.roeung@gmail.com>
2023-09-07 15:58:16 -0400

    fix(product-landing): Fix various blocks and section used within MDX landing page (#326)
    * fix(product-landing): clean up code & adjust accordion
    
    * fix(product-landing): add more changes to accordion
    
    * feat(product-event-template): add stats with illustration style and linkable block
    
    * fix(product-landing): clean up linkable card block script and styles
    
    * fix(product-landing): linkable cards fix paragraph size
    
    * fix(product-landing): remove un-useful code made in the content band checklist
    
    * fix(product-landing): remove content band checkmark and reuse content intro block instead
    
    * fix(product-landing): adjust content intro block
    
    * fix(product-landing): add checkmark style to Content Intro block
    
    * fix(product-landing): fix linkable block card styling
    
    * fix(product-landing): fix lint issues
    
    * fix(product-landing): add theme to Accordion block
    
    * fix(product-landing): fix svg icons
    
    * fix(product-landing): address design QA feedback
    
    * fix(product-landing): reorder font size vars
    
    * fix(product-landing): fix secondary button play icon color

--- blocks/content-intro/content-intro.js
+++ blocks/content-intro/content-intro.js
@@ -1,26 +1,53 @@
 export default function decorate(block) {
+  // Get the block name from the data attribute
+  const blockName = block.getAttribute('data-block-name');
+
+  if (!blockName) {
+    return;
+  }
+
+  // Define class names for reuse
+  const innerClassName = `${blockName}__inner`;
+  const colClassName = `${blockName}__col`;
+  const eyebrowClassName = `${blockName}__eyebrow`;
+
+  // Loop through each child element of the block
   [...block.children].forEach((element) => {
-    element.classList.add('content-intro__inner');
+    // Add the inner class to the element
+    element.classList.add(innerClassName);
 
-    // Find all the div elements within the elements with class 'content-intro__inner'
-    const innerElements = element.querySelectorAll('.content-intro__inner div');
+    // Find all div elements within the inner content
+    const innerElements = element.querySelectorAll(`.${innerClassName} div`);
 
-    // Add the class 'content-intro__col' and append a number to each of these div elements
     let counter = 1;
     innerElements.forEach((divElement) => {
-      const newClass = `content-intro__col-${counter}`;
-      divElement.classList.add('content-intro__col', newClass);
-      counter += 1; // Use prefix notation to increment the counter
+      // Add column class and numbered class to each div
+      const newColClass = `${colClassName}-${counter}`;
+      divElement.classList.add(colClassName, newColClass);
+      counter += 1;
 
-      // Find the <h2> element inside each .content-intro__inner div
+      // Find the h2 element inside the div
       const h2Element = divElement.querySelector('h2');
 
       if (h2Element) {
-        // Find the previous sibling of <h2> element
+        // Find the previous sibling of h2 element
         const previousElement = h2Element.previousElementSibling;
 
+        // Add eyebrow class to preceding paragraph if it exists
         if (previousElement && previousElement.tagName.toLowerCase() === 'p') {
-          previousElement.classList.add('content-intro__eyebrow');
+          previousElement.classList.add(eyebrowClassName);
+        }
+      } else if (innerElements.length > 0) {
+        const firstDivElement = innerElements[0];
+
+        // Check if the firstDivElement doesn't contain an h2 element
+        if (!firstDivElement.querySelector('h2')) {
+          // Create a new paragraph for eyebrow content
+          const contentIntroEyebrow = document.createElement('p');
+          contentIntroEyebrow.classList.add(eyebrowClassName);
+          contentIntroEyebrow.textContent = firstDivElement.textContent;
+          firstDivElement.textContent = '';
+          firstDivElement.appendChild(contentIntroEyebrow);
         }
       }
     });
--- blocks/cta/cta.js
+++ blocks/cta/cta.js
@@ -1,31 +1,39 @@
 import { createTag } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  const col1 = block.firstElementChild?.children.item(0);
-  // group buttons
-  const buttonGroup = createTag('div', { class: 'button-group' });
-  const buttons = [...col1.querySelectorAll('.button-container')];
-
-  buttons.forEach((button) => {
-    const isPrimary = button.querySelector('strong');
-    const isSecondary = button.querySelector('em');
-    // position button
-    if (isPrimary) {
-      button.style.order = '1';
-    } else if (isSecondary) {
-      button.style.order = '2';
-    } else {
-      button.style.order = '3';
-    }
-    buttonGroup.append(button);
-  });
-
-  if (buttonGroup.children.length) {
-    const pictures = col1.querySelectorAll('p.only-picture');
-    if (pictures.length > 0) {
-      pictures[pictures.length - 1].insertAdjacentElement('beforeBegin', buttonGroup);
-    } else {
-      col1.append(buttonGroup);
-    }
+  const blockName = block.getAttribute('data-block-name');
+  if (!blockName) {
+    return;
   }
+
+  [...block.children].forEach((element) => {
+    element.classList.add(`${blockName}__inner`);
+
+    // Group buttons
+    const buttonGroup = createTag('div', { class: 'button-group' });
+    const buttons = element.querySelectorAll('.button-container');
+
+    buttons.forEach((button) => {
+      const isPrimary = button.querySelector('strong');
+      const isSecondary = button.querySelector('em');
+      // Position button
+      if (isPrimary) {
+        button.style.order = '1';
+      } else if (isSecondary) {
+        button.style.order = '2';
+      } else {
+        button.style.order = '3';
+      }
+      buttonGroup.append(button);
+    });
+
+    if (buttonGroup.children.length) {
+      const pictures = element.querySelectorAll('p.only-picture');
+      if (pictures.length > 0) {
+        pictures[pictures.length - 1].insertAdjacentElement('beforeBegin', buttonGroup);
+      } else {
+        element.append(buttonGroup);
+      }
+    }
+  });
 }
