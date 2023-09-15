a60bcae201e9bff0cc87b2255ec76c544781cb03
sahmad-merative <135624814+sahmad-merative@users.noreply.github.com>
2023-09-15 19:14:30 +0530

    feature(author-bio): Dynamically adding author bio (#332)
    * feature(author-bio): Dynamically adding author bio
    
    * feature(author-bio): design review fixes
    
    * feature(author-bio): Addressed review comments
    
    ---------
    
    Co-authored-by: Putra Bonaccorsi <putra.roeung@gmail.com>

--- blocks/author-bio/author-bio.js
+++ blocks/author-bio/author-bio.js
@@ -1,34 +1,73 @@
-import { createTag, getMetadata } from '../../scripts/scripts.js';
+import { createTag, getMetadata, lookupPeople } from '../../scripts/scripts.js';
 import authorsParser from '../../scripts/authors-parser.js';
 
-export default function decorate(block) {
+export default async function decorate(block) {
   const defaultAuthorImageSrc = '../../styles/images/AuthorPlaceholder.svg';
   const defaultAuthor = 'Merative';
 
-  let authors = getMetadata('authors');
+  let authors = [];
   const nameField = 'name';
   const titleField = 'title';
   const imageField = 'image';
   const content = [];
 
-  if (!authors) {
+  // get author details from people/query-index
+  const pathnames = [...block.querySelectorAll('a')].map((a) => {
+    const url = new URL(a.href);
+    if (url.hostname.endsWith('.page') || url.hostname.endsWith('.live') || url.hostname.endsWith('merative.com') || url.hostname.startsWith('localhost')) return url.pathname;
+    return a.href;
+  });
+
+  const peopleList = await lookupPeople(pathnames);
+  if (peopleList.length) {
+    peopleList.forEach((people) => {
+      const author = {};
+      let isAuthorFlag = false;
+
+      // Add name
+      if (people.title && people.title !== '0') {
+        author[nameField] = people.title;
+        isAuthorFlag = true;
+      }
+      // Add image
+      if (people.image !== '0' && people.title !== '0') {
+        const url = new URL(people.image, window.location.href);
+        author[imageField] = url.pathname;
+        isAuthorFlag = true;
+      }
+      // Add title
+      if (people['display-title'] && people['display-title'] !== '0') {
+        author[titleField] = people['display-title'];
+        isAuthorFlag = true;
+      }
+
+      if (isAuthorFlag) {
+        authors.push(author);
+      }
+    });
+  }
+
+  // get author details from meta-data
+  if (authors.length === 0) {
+    const authorMetaData = getMetadata('authors');
+    authors = authorsParser(authorMetaData);
+  }
+
+  // set default author details if authors are not fetched
+  if (!authors || authors.length === 0) {
     authors = [{
       [nameField]: defaultAuthor,
       [imageField]: defaultAuthorImageSrc,
     }];
   }
 
-  const authorsQuantity = authors.length;
-  authors = authorsParser(authors);
-  block.innerHTML = authorsQuantity > 1 ? '' : '';
+  block.innerHTML = '';
 
   authors.forEach((author) => {
     const authorContainer = createTag('div', { class: 'author-container' });
     const authorImageTag = createTag('img', { class: 'author-img', alt: 'author-image' });
     authorImageTag.setAttribute('src', author[imageField] || defaultAuthorImageSrc);
-    authorImageTag.setAttribute('alt', 'author-image');
     const authorNameTag = createTag('div', { class: 'author-name' });
-    authorNameTag.innerHTML = authorsQuantity === 1 ? '' : '';
     const authorNameHighlight = createTag('span', { class: 'author-name-highlight' });
     const authorTitleHighlight = createTag('span', { class: 'author-title-highlight' });
     authorNameHighlight.innerHTML = author[titleField] ? `${author[nameField]},` : author[nameField];
